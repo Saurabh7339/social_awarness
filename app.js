f230a9f9643d6c6792bef961ead1f06bee546284
saurabh kumar <saurabhkr7339@gmail.com>
2020-12-19 17:09:28 +0530

    added routes

--- app.js
+++ app.js
@@ -11,7 +11,11 @@ connectDB();
 app.get('/',(req,res)=>{
     console.log('the api is working ');
     res.send('<p>this is working </p>');
-})
+});
+app.use('/api/users',require('./routes/api/users'));
+app.use('/api/auth',require('./routes/api/auth'));
+app.use('/api/profile',require('./routes/api/profile'));
+app.use('/api/posts',require('./routes/api/posts'));
 
 
 
