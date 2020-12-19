e970ef0154a33f7ef072e002b8d25d792a303bde
saurabh kumar <saurabhkr7339@gmail.com>
2020-12-19 14:59:44 +0530

    server and packages

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
+
+mongoose.connect('mongodb+srv://dante:123@cluster0.e4akl.mongodb.net/<dbname>?retryWrites=true&w=majority');
+
+app.get('/',(req,res)=>{
+    console.log('the api is working ');
+    res.send(<p>this is working </p>);
+})
 
 
 
